4985364777cc9fd2bbc929bfaa01893a4e0a2cb5
Hampus Persson <hampus@vaam.io>
2025-10-05 11:27:11 +0200

    fix db

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,7 +3,7 @@ import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 
 const client = createClient({
-  url: process.env.DATABASE_URL ?? "file:./data/app.sqlite",
+  url: process.env.DATABASE_URL ?? "file:../../data/app.sqlite",
 });
 
 export const db = drizzle(client, { schema });
